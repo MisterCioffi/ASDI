5a50697417365fe3de5cff17dd923c2d0fd7589f
Antonio <digiorgio.antonio18@gmail.com>
2026-03-02 14:06:34 +0100

    fine esercizio 5.2

--- Esercizi/Esercizi_Reti_Sequenziali_Elementari/Esercizio_5.2/Esercizio_5.2.runs/impl_1/rundef.js
+++ Esercizi/Esercizi_Reti_Sequenziali_Elementari/Esercizio_5.2/Esercizio_5.2.runs/impl_1/rundef.js
@@ -23,7 +23,7 @@ eval( EAInclude(ISEJScriptLib) );
 
 
 // pre-commands:
-ISETouchFile( "init_design", "begin" );
+ISETouchFile( "write_bitstream", "begin" );
 ISEStep( "vivado",
          "-log Cronometro_Board.vdi -applog -m64 -product Vivado -messageDb vivado.pb -mode batch -source Cronometro_Board.tcl -notrace" );
 
